00abc81cff08cc280c0d39d7e6e1667f928d0ae2
MateiIonutEduard <ionuteduardm@gmail.com>
2021-01-02 18:40:33 +0200

    Update project files.

--- MovieStorm/wwwroot/js/index.js
+++ MovieStorm/wwwroot/js/index.js
@@ -9,7 +9,7 @@
         cache: false,
         success: function (data) {
             for (var i = 0; i < data.length; i++) {
-                var img = `<img src='Home/Show/?path=${data[i].preview}' height='250' width='200' style='margin-top: 30px;margin-left:30px;margin-right:20px;' />`;
+                var img = `<img src='/Home/Show/?path=${data[i].preview}' height='250' width='200' style='margin-top: 30px;margin-left:30px;margin-right:20px;' />`;
                 var name = `<input type='submit' name='submit' class='btn btn-outline-info' style='display:block;width:200px;padding-bottom:15px;margin-bottom:30px;margin-left:30px;margin-right:20px;' value='${data[i].name}'/>`;
                 var noob = `<div class='noob'>${name}</div>`;
 
@@ -33,7 +33,7 @@
             var j = 1;
 
             for (var i = 0; i < data.length; i++) {
-                var img = `<img src='Home/Show/?path=${data[i].preview}' height='250' width='200' style='margin-top: 30px;margin-left:30px;margin-right:20px;' />`;
+                var img = `<img src='/Home/Show/?path=${data[i].preview}' height='250' width='200' style='margin-top: 30px;margin-left:30px;margin-right:20px;' />`;
                 var name = `<input type='submit' name='submit' class='btn btn-outline-info' style='display:block;width:200px;padding-bottom:15px;margin-bottom:30px;margin-left:30px;margin-right:20px;' value='${data[i].name}'/>`;
                 var noob = `<div class='noob'>${name}</div>`;
 
