cc5279e144ca4cb42c9e4201173176454ef38809
MateiIonutEduard <ionuteduardm@gmail.com>
2021-01-10 15:53:39 +0200

    Add project files.

---
+++ MovieStorm/wwwroot/js/signup.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(() => {
+    $(document).on("submit", "#signup", (e) => {
+        e.preventDefault();
+        
+        var buffer = {
+            'username': $("#name").val(),
+            'password': $("#pass").val(),
+            'address': $("#address").val(),
+            'logo': $('#logo').get(0).files[0],
+            'admin': $('#admin').val()
+        };
+
+        $.ajax({
+            url: '/Account/Signup',
+            type: 'post',
+            data: buffer,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: data => {
+                setTimeout(() => {
+                    location.href = '/Home/Index';
+                }, 1000);
+            },
+            async: true
+        });
+    });
+});
